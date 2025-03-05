2615d2214d4f2d1fc426b9c61f115466c369afbf
Vamshi Krishna <vamshibusiness07@gmail.com>
2025-03-05 17:35:05 +0530

    Delete vamshi.protoflio directory
    Signed-off-by: Vamshi Krishna <vamshibusiness07@gmail.com>

--- vamshi.protoflio/Script.js
+++
@@ -1,9 +0,0 @@
-function openSlider(artImg, originalImg) {
-    document.getElementById("sliderFront").src = artImg;
-    document.getElementById("sliderBack").src = originalImg;
-    document.getElementById("imageSlider").style.display = "flex";
-}
-
-function closeSlider() {
-    document.getElementById("imageSlider").style.display = "none";
-}
